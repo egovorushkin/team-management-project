23bc703ab076366840dcea470c0153327032160e
Evgenii Govorushkin <e.govorushkin@gmail.com>
2020-09-12 19:32:21 +0300

    added backend (springboot)

---
+++ backend/spring-boot-team-management/src/main/java/com/egovorushkin/teammanagement/SpringBootTeamManagementApplication.java
@@ -0,0 +1,13 @@
+package com.egovorushkin.teammanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTeamManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTeamManagementApplication.class, args);
+	}
+
+}
---
+++ backend/spring-boot-team-management/src/test/java/com/egovorushkin/teammanagement/SpringBootTeamManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egovorushkin.teammanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTeamManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
