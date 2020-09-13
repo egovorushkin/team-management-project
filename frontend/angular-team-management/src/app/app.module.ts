b2de1b82d9d3a87e43e3d6b09e24ad7fc290cc55
Evgenii Govorushkin <e.govorushkin@gmail.com>
2020-09-13 20:49:09 +0300

    added frontend

--- frontend/angular-team-management/src/app/app.module.ts
+++ frontend/angular-team-management/src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { EmployeeService } from './services/employee.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
